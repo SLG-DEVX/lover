98465b716bbb8d4db5c9be28b651293f0d6adc87
SLG-DEVX <prudenceelise2009@gmail.com>
2025-08-10 22:36:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8000;
+
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+});
+
+module.exports = app;
